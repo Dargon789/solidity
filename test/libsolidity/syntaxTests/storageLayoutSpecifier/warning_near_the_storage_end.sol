529795019c766e2aced54455ce613254b1865df9
Matheus Aguiar <matheus.pit@gmail.com>
2025-03-04 00:19:40 -0300

    Warn when layout base is near the end of storage

--- test/libsolidity/syntaxTests/storageLayoutSpecifier/warning_near_the_storage_end.sol
+++ test/libsolidity/syntaxTests/storageLayoutSpecifier/warning_near_the_storage_end.sol
@@ -4,3 +4,5 @@ contract C layout at 2**256 - 2**65 {
     uint[2**63] y;
 }
 // ----
+// Warning 3495: (11-35): This contract is very close to the end of storage. This limits its future upgradability.
+// Warning 3495: (50-74): This contract is very close to the end of storage. This limits its future upgradability.
